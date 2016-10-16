46bafe8a6e82b6f022859335a938df8299fefee7
June Rhodes <jrhodes@redpointgames.com.au>
2016-10-16 12:00:30 +1100

    Add a buildOptimized function for Node.js projects

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var runProcessWithOutput = require('orchestration-util-process').runProcessWithOutput;
+var runProcessWithOutputAndWorkingDirectory = require('orchestration-util-process').runProcessWithOutputAndWorkingDirectory;
 var spawn = require('child_process').spawn;
 var fs = require('fs');
+var ncp = require('ncp').ncp;
+var async = require('async');
+var targz = require('tar.gz');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var rimraf = require('rimraf');
+
+ncp.limit = 16;
 
 function getDockerfile(config, environment) {
   var version = config.dockerImageVersion || config.package.version;
@@ -190,6 +199,257 @@ function build(config, environment, callback) {
   });
 }
 
+function buildOptimized(config, environment, callback) {
+  if (!(config.orchestration.packageType == null || config.orchestration.packageType == 'nodejs')) {
+    callback(new Error('buildOptimized only worked on Node.js Docker images'));
+    return;
+  }
+
+  var version = config.dockerImageVersion || config.package.version;
+
+  var dockerPrefix = config.cluster.environments[environment].dockerImagePrefix;
+
+  if (dockerPrefix == "" || dockerPrefix[dockerPrefix.length - 1] != "/") {
+    callback(new Error("Docker prefix in cluster does not end with a slash, refusing to push to potentially public location!"));
+    return;
+  }
+
+  mkdirp('_docker/bundle', (err) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    var createDirectory = function(dir, callback) {
+      fs.access(dir, fs.F_OK, (err) => {
+        if (err) {
+          fs.mkdir(dir, (err) => {
+            if (err) {
+              callback(err);
+              return;
+            }
+
+            callback(null);
+          });
+        } else {
+          callback(null);
+        }
+      });
+    };
+
+    var copyAllFilesToWorkingDirectory = function(callback) {
+      var filesToCopy = [
+        //'node_modules'
+      ];
+
+      for (var i = 0; i < config.orchestration.files.length; i++) {
+        var value = config.orchestration.files[i];
+        if (typeof value == "string") {
+          // Always include
+          filesToCopy.push(value);
+        } else {
+          if (value.env == environment) {
+            filesToCopy.push(value.name);
+          }
+        }
+      }
+
+      async.map(
+        filesToCopy,
+        (item, callback) => {
+          console.log('copy ' + item + ' ...');
+          mkdirp(path.dirname('_docker/bundle/' + item), (err) => {
+            if (err) {
+              callback(err);
+              return;
+            }
+            
+            ncp(item, '_docker/bundle/' + item, {clobber: false, modified: true}, (err) => {
+              if (err) {
+                callback(err);
+                return;
+              }
+
+              console.log('copy ' + item + ' done');
+              callback();
+            });
+          });
+        },
+        callback);
+    };
+
+    var copyNpmCacheToWorkingDirectory = function(callback) {
+      var cachePath = null;
+      if (process.platform == 'win32') {
+        cachePath = process.env['APPDATA'] + '\\npm-cache';
+      } else {
+        cachePath = process.env['HOME'] + '/.npm';
+      }
+
+      console.log('copy npm cache ...');
+      mkdirp(path.dirname('_docker/bundle/_npmcache'), (err) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+        
+        ncp(cachePath, '_docker/bundle/_npmcache', {clobber: false, modified: true}, (err) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          console.log('copy npm cache done');
+          callback();
+        });
+      });
+    }
+    
+    var writeVersionlessFile = function(callback) {
+      console.log('write versionless file ...');
+      var versionless = getVersionlessPackageJson();
+
+      var writePackageJson = function(callback) {
+        fs.writeFile('_docker/bundle/package.json', versionless, (err) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          console.log('write versionless file done');
+          callback();
+        });
+      };
+
+      fs.access('_docker/bundle/package.json', fs.F_OK, (err) => {
+        if (err) {
+          writePackageJson(callback);
+        } else {
+          fs.readFile('_docker/bundle/package.json', 'utf8', (err, data) => {
+            if (err) {
+              callback(err);
+              return;
+            }
+
+            if (data == versionless) {
+              console.log('write versionless file done');
+              callback();
+              return;
+            }
+
+            writePackageJson(callback);
+          });
+        }
+      });
+    };
+
+    var copyVersionedFile = function(callback) {
+      console.log('copy versioned file ...');
+      ncp('package.json', '_docker/bundle/package.json.versioned', {clobber: false, modified: true}, (err) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        console.log('copy versioned file done');
+        callback();
+      });
+    };
+
+    var copyGitCredentials = function(callback) {
+      if (config.orchestration.privateRepoKey != null) {
+        console.log('copy git credentials ...');
+        ncp(config.orchestration.privateRepoKey, '_docker/bundle/id_rsa', {clobber: false, modified: true}, (err) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          console.log('copy git credentials done');
+          callback();
+        });
+      } else {
+        callback(null);
+      }
+    };
+
+    async.parallel([
+      writeVersionlessFile,
+      copyNpmCacheToWorkingDirectory,
+      copyAllFilesToWorkingDirectory,
+      copyVersionedFile,
+      copyGitCredentials
+    ], (err) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      console.log('build docker image ...');
+
+      var keyscanCommands = [];
+      for (var i = 0; i < config.orchestration.privateRepoHosts.length; i++) {
+        keyscanCommands.push("ssh-keyscan -H " + config.orchestration.privateRepoHosts[i] + " >> ~/.ssh/known_hosts");
+      }
+
+      var commands = [
+        // Copy Git credentials if needed
+        '(if [ -e id_rsa ]; then mkdir -pv /root/.ssh; mv id_rsa /root/.ssh/id_rsa; chmod 0600 /root/.ssh/id_rsa; fi)',
+        // Perform SSH keyscans for hosts
+      ].concat(keyscanCommands).concat([
+        // Move cache
+        'mv _npmcache /root/.npm',
+        'NODE_ENV=production npm install -g npm-offline --production --cache-min 999999',
+        '(npm-offline &)',
+        'npm config set registry http://localhost:12644/',
+        // Install any required modules
+        'NODE_ENV=production npm install --production --cache-min 999999',
+        // Move versioned file in place
+        'mv package.json.versioned package.json',
+        // Delete any Git credentials
+        '(if [ -e /root/.ssh/id_rsa ]; then rm /root/.ssh/id_rsa; fi)'
+      ]);
+
+      var exposeStatements = [];
+      if (config.orchestration.services != null) {
+        for (var i = 0; i < config.orchestration.services.length; i++) {
+          var value = config.orchestration.services[i][environment];
+          exposeStatements.push("EXPOSE " + value.containerPort + "\n");
+        }
+      }
+
+      var dockerfile = `
+FROM node:latest
+WORKDIR /srv
+ADD bundle /srv
+RUN ` + commands.join(' && ') + `
+` + exposeStatements + `
+CMD NODE_ENV=` + environment + ` node ` + config.package.main + `
+`;
+
+      fs.writeFile('_docker/Dockerfile', dockerfile, (err) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        console.log('wrote docker file, starting docker build ...');
+
+        runProcessWithOutput(
+          'docker',
+          [
+            'build',
+            '-t',
+            dockerPrefix + config.package.name + ":" + version,
+            '_docker'
+          ],
+          callback
+        );
+      });
+    });
+  });
+}
+
 function push(config, environment, callback) {
   var version = config.dockerImageVersion || config.package.version;
 
@@ -338,6 +598,7 @@ function testLocalOpts(config, environment, opts, callback) {
 
 module.exports = {
   build: build,
+  buildOptimized: buildOptimized,
   push: push,
   testLocal: testLocal,
   testLocalOpts: testLocalOpts
